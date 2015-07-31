16fec0a5806852b270cdafddfb276d6efd099ac4
Matt Schick <matt@schickm.com>
2015-07-31 13:39:24 -0500

    make nice login form for client

--- src/js/index.js
+++ src/js/index.js
@@ -7,43 +7,51 @@ var HtmlPlayer = require('./html-player');
 
 var params = queryString.parse(location.search);
 
-var socket = io(params.controller, {
-    forceNew: true
-});
-
-var player = new HtmlPlayer(document.querySelector('#main'));
-
-player.on('mediaTransitioning', function(mediaObject) {
-    socket.emit('mediaTransitioning', mediaObject.getId());
-});
-player.on('mediaDone', function(mediaObject) {
-    socket.emit('mediaDone', mediaObject.getId());
-});
-
-function handleError (err) {
-    if (err) {
-        throw err;
-    }
+if (params.controller && params.sceneId) {
+    connect(params.controller, params.sceneId);
+} else {
+    showForm();
 }
 
-socket.on('connect', function() {
-    console.log('socket connected');
+function connect (controllerUrl, sceneId) {
+    var socket = io(controllerUrl, {
+        forceNew: true
+    });
 
-    socket.on('showMedia', player.show.bind(player));
+    var player = new HtmlPlayer(document.querySelector('#main'));
 
-    socket.emit('playScene', params.sceneId);
-});
+    player.on('mediaTransitioning', function(mediaObject) {
+        socket.emit('mediaTransitioning', mediaObject.getId());
+    });
+    player.on('mediaDone', function(mediaObject) {
+        socket.emit('mediaDone', mediaObject.getId());
+    });
 
-socket.on('connect_error', handleError);
-socket.on('reconnect_error', handleError);
-socket.on('connect_error', handleError);
+    function handleError (err) {
+        if (err) {
+            throw err;
+        }
+    }
 
+    socket.on('connect', function() {
+        console.log('socket connected');
 
+        socket.on('showMedia', player.show.bind(player));
 
+        socket.emit('playScene', params.sceneId);
+    });
 
-// var React = require('react');
-// var Router = require('./viewer-router.jsx');
-// var HubSendActions = require('./actions/hub-send-actions');
+    socket.on('connect_error', handleError);
+    socket.on('reconnect_error', handleError);
+    socket.on('connect_error', handleError);
+}
 
-// HubSendActions.tryTokenLogin();
-
+function showForm () {
+    document.querySelector('#connection').classList.add('show');
+    
+    var form = document.querySelector('#connectionForm');
+    ['controller', 'sceneId'].forEach(function(input) {
+        form[input].value = params[input] || '';
+    });
+    
+}
